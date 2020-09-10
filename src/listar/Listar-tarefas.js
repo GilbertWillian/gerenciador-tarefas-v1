68de532a9a921f743c51c4160751c0608f525566
GilbertWillian <gilbertowillian99@gmail.com>
2020-09-10 15:29:03 -0300

    Definindo estrutura base do componenete de Listar Tarefas

--- src/listar/Listar-tarefas.js
+++ src/listar/Listar-tarefas.js
@@ -1,16 +1,37 @@
 import React from "react";
-import { A } from 'hookrouter';
+import { A } from "hookrouter";
+import { Table } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Habilita o uso de icones
+import { faPlus } from "@fortawesome/free-solid-svg-icons"; // Icone que será usado
 
 function ListarTarefas() {
-  return(
+  return (
+    <div className="text-center">
+      <h3>Tarefas a fazer</h3>
 
-    <A href="/cadastrar"
-    className="btn btn-success btn-sm">
-      Nova tarefa
-    </A>
+      <Table striped bordered hover responsive data-testid="tabela">
+        <thead>
+          <tr>
+            <th>Tarefa</th>
+            <th>
+              <A
+                href="/cadastrar"
+                className="btn btn-success btn-sm"
+                data-testid="btn-nova-tarefa"
+              >
+                <FontAwesomeIcon icon={faPlus} />
+                &nbsp; {/* Usado para dar espaços */}
+                Nova Tarefa
+              </A>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </Table>
+    </div>
   );
-  
-
 }
 
 export default ListarTarefas;
