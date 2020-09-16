ade2d90d35fd4fda8d11dc2aa88e30d40f610b2c
GilbertWillian <gilbertowillian99@gmail.com>
2020-09-16 17:29:14 -0300

    Adiconando Testes Unitários do componente de Remover Tarefa

---
+++ src/listar/Remover-tarefa.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RemoverTarefa from './Remover-tarefa';
+import Tarefa from '../models/tarefa.model';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('teste do componente de remoção de tarefas', () => {
+
+  const nomeTarefa = 'Tarefa de teste';
+  const tarefa = new Tarefa(1, nomeTarefa, false);
+
+  it('deve renderizar o componente sem erros', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <RemoverTarefa
+        tarefa={tarefa}
+        recarregarTarefas={() => false} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('deve exibit a modal', () => {
+    const { getByTestId } = render(
+      <RemoverTarefa
+        tarefa={tarefa}
+        recarregarTarefas={() => false} />
+    );
+    fireEvent.click(getByTestId('btn-abrir-modal'));
+    expect(getByTestId('modal')).toHaveTextContent(nomeTarefa);
+  });
+
+  it('deve remover uma tarefa', () => {
+    localStorage['tarefas'] = JSON.stringify([tarefa]);
+    const { getByTestId } = render(
+      <RemoverTarefa
+        tarefa={tarefa}
+        recarregarTarefas={() => false} />
+    );
+    fireEvent.click(getByTestId('btn-abrir-modal'));
+    fireEvent.click(getByTestId('btn-remover'));
+    const tarefasDB = JSON.parse(localStorage['tarefas']);
+    expect(tarefasDB.length).toBe(0);
+  });
+
+})
