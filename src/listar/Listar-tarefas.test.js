4b2a21a3e4b84918b033748c3847816edfa1aed1
GilbertWillian <gilbertowillian99@gmail.com>
2020-09-17 22:03:18 -0300

    Adicionando testes unitários da funcionalidade de filtrar tarefas

--- src/listar/Listar-tarefas.test.js
+++ src/listar/Listar-tarefas.test.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ListarTarefas from './Listar-tarefas';
+import React from "react";
+import ReactDOM from "react-dom";
+import ListarTarefas from "./Listar-tarefas";
+import Tarefa from "../models/tarefa.model";
+import { render, fireEvent, getByTestId } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-describe('Teste do componente de listagem de tarefas', () => {
+describe("Teste do componente de listagem de tarefas", () => {
+  const nomePrimeiraTarefa = "Primeira Tarefa";
+  const nomeSegundaTarefa = "Segunda Tarefa";
+  const nomeTerceiraTarefa = "Terceira Tarefa";
 
-  it('deve renderizar o componente sem erros', () => {
-    const div = document.createElement('div');
+  beforeEach(() => {
+    // Acontece antes do testes, usado para criar dados
+    localStorage["tarefas"] = JSON.stringify([
+      new Tarefa(1, nomePrimeiraTarefa, false),
+      new Tarefa(2, nomeSegundaTarefa, false),
+      new Tarefa(3, nomeTerceiraTarefa, false),
+    ]);
+  });
+
+  afterEach(() => {
+    delete localStorage["tarefas"];
+  });
+
+  it("deve renderizar o componente sem erros", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<ListarTarefas />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
-})
+  it("deve exibir uma tabela contendo 3 tabelas", () => {
+    const { getByTestId } = render(<ListarTarefas />);
+    const tabela = getByTestId("tabela");
+    expect(tabela).toHaveTextContent(nomePrimeiraTarefa);
+    expect(tabela).toHaveTextContent(nomeSegundaTarefa);
+    expect(tabela).toHaveTextContent(nomeTerceiraTarefa);
+  });
+
+  it("deve filtrar os dados da tabela de tarefas", () => {
+    const { getByTestId } = render(<ListarTarefas />);
+    fireEvent.change(getByTestId("txt-tarefa"), {
+      target: { value: nomePrimeiraTarefa },
+    });
+    const tabela = getByTestId("tabela");
+    expect(tabela).toHaveTextContent(nomePrimeiraTarefa);
+    expect(tabela).not.toHaveTextContent(nomeSegundaTarefa);
+    expect(tabela).not.toHaveTextContent(nomeTerceiraTarefa);
+  });
+});
